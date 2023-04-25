fa52e5b2ea8056ff538f16cf3311e09c0df1ee30
Emiliano Policardo <emilianopolicardo@gmail.com>
2023-04-24 21:46:34 -0300

    City

---
+++ Sales.Shared/Entities/City.cs
@@ -0,0 +1,10 @@
+namespace Sales.Shared.Entites
+{
+    public class City{
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PostalCode { get; set; }
+     
+    }
+}
