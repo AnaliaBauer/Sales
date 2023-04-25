cb9d1b6fc77ebb1b2aec1261cbbd3154c81349fd
AnaliaBauer <bauerany@gmail.com>
2023-04-24 22:22:33 -0300

    prueba

--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -5,7 +5,7 @@ namespace Sales.Shared.Entites
         public int Id { get; set; }
         public string Name { get; set; }
         public string PostalCode { get; set; }
-        public string AnotherProp {get;set; }
+        public string AnotherThing {get;set; }
      
     }
 }
